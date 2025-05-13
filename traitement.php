f981263d90c17c8dc51a30737b969365811ebc20
brinahfah <ebikie4@gmail.com>
2025-05-13 10:32:42 +0200

    large

--- traitement.php
+++ traitement.php
@@ -1,14 +1,15 @@
 <?php
 // Bug 1: 
-$nom = $_POST['nom'];
-$email = $_POST['email'];
-$age = $_POST['age'];
-$message = $_POST['message'];
+
 
 // Bug 2:
 IF ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    $nom = $_POST['nom'];
+    $email = $_POST['email'];
+    $age = $_POST['age'];
+    $message = $_POST['message'];
  // Vérification des données
-     if (empty($nom) || empty($email) || empty($age) || empty($message)) {
+     if (empty($message)) {
         echo "Tous les champs sont obligatoires";
         exit;
     }else if( $age < 0) {
